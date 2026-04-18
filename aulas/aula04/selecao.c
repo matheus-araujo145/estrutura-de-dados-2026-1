869380f86656c6c4a03b424d554112d837ee6ce0
Matheus de Araújo Barreto <matheus.a.barreto@iesb.edu.br>
2026-04-18 11:14:32 -0300

    Concluindo aula04 e pratica04

--- aulas/aula04/selecao.c
+++ aulas/aula04/selecao.c
@@ -1,15 +1,57 @@
-#include"selecao.h"
+#include "selecao.h"
 
-#define tamanho 10
+int busca_sequencial(int vetor[], int n, int valor) 
+{
+    for(int i = 0; i < n; i++) 
+    {
+        if(vetor[i] == valor)
+        return i;
+    }
+    return -1;    
+}
 
-int busca_sequencial(int v[], int n, int valor){
+void troca(int *a, int *b) {
+    int aux = *a;
+    *a = *b;
+    *b = aux;
+}
 
-    for(int i = 0; i < n; i++){ // O(n)
-
-        if (v[i] == valor){
-
-            return i;
+int particionar_vetor(int v[], int inicio, int fim) 
+{
+    int pivo = v[fim];
+    int i = inicio - 1;
+    for (int j = inicio + 1; j < fim; j++) 
+    {
+        if(v[j] <= pivo) {
+            i++;
+            troca(&v[i], &v[j]);
         }
     }
-    return -1;
+    troca(&v[i + 1], &v[fim]);
+    return i + 1;
+}
+
+int quick_select(int v[], int inicio, int fim, int valor)
+{
+    if (inicio > fim)
+        return -1; // não encontrou
+
+    if (inicio == fim)
+    {
+        if (v[inicio] == valor)
+            return inicio;
+        else
+            return -1;
+    }
+
+    int pivo = particionar_vetor(v, inicio, fim);
+
+    if (v[pivo] == valor)
+        return pivo;
+
+    else if (v[pivo] < valor)
+        return quick_select(v, pivo + 1, fim, valor);
+
+    else
+        return quick_select(v, inicio, pivo - 1, valor);
 }
--- aulas/aula04/selecao.h
+++ aulas/aula04/selecao.h
@@ -1,7 +1,10 @@
-#ifndef selecao_h
-#define selecao_h
+#ifndef SELECAO_H
+#define SELECAO_H
 
-int busca_sequencial(int v[], int n, int valor);
+int busca_sequencial(int vetor[], int n, int valor);
 
+int quick_select(int v[], int inicio, int fim, int valor);
+int particionar_vetor(int v[], int inicio, int fim);
+void troca(int *a,int *b);
 
 #endif
